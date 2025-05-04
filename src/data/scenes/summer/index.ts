5993dcb45b598a4dc291e86c959fcb9a260ab7b5
hazelskykin <candypears@gmail.com>
2025-05-03 23:10:05 -0400

    Update index.ts

--- src/data/scenes/summer/index.ts
+++ src/data/scenes/summer/index.ts
@@ -2,10 +2,18 @@
 import { Scene } from '../../../types/game';
 import introScenes from './intro';
 import festivalScenes from './festival';
+import minigameScenes from './minigames';
+import conclusionScenes from './conclusion';
+import characterSelections from './characterSelection';
+import characterVisits from './characterVisits';
 
 const summer: Record<string, Scene> = {
   ...introScenes,
+  ...characterSelections,
+  ...characterVisits,
   ...festivalScenes,
+  ...minigameScenes,
+  ...conclusionScenes,
   // Future sections will be added here:
   // ...minigameScenes,
   // ...conclusionScenes,
