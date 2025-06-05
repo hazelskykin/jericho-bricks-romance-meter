1db24928a37f331905d4cedd93b29d28b6a3b8e8
hazelskykin <candypears@gmail.com>
2025-06-05 13:15:21 -0500

    Update characters.ts

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -5,6 +5,7 @@ export const characters: Record<string, Character> = {
   xavier: {
     id: 'xavier',
     name: 'Xavier',
+    lastname: 'Chen',
     role: 'Solvitor',
     description: 'The engineering and technological bedrock of the team.',
     affection: 0,
@@ -15,6 +16,7 @@ export const characters: Record<string, Character> = {
   navarre: {
     id: 'navarre',
     name: 'Navarre',
+    lastname: 'Ellis',
     role: 'Bellfox',
     description: 'A natural networker with a flirtatious and persuasive style.',
     affection: 0,
@@ -25,6 +27,7 @@ export const characters: Record<string, Character> = {
   etta: {
     id: 'etta',
     name: 'Etta',
+    lastname: 'Montgomery',
     role: 'Diva',
     description: 'Confident to the point of arrogance, with a narrow focus on goals.',
     affection: 0,
@@ -35,6 +38,7 @@ export const characters: Record<string, Character> = {
   senara: {
     id: 'senara',
     name: 'Senara',
+    lastname: 'Kapoor',
     role: 'Gnarus',
     description: 'The enigmatic intellectual, a philosopher that bridges knowledge with inquiry.',
     affection: 0,
@@ -48,6 +52,7 @@ export const characters: Record<string, Character> = {
 export const maven: Character = {
   id: 'maven',
   name: 'Maven',
+  lastname: 'Gray',
   role: 'Versa',
   description: 'Uncertain of her strengths, dealing with imposter syndrome.',
   affection: 0,
